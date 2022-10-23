7abd3cfe4b7fbf9a5f4d3d8968222506bb7825c1
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-10-23 22:55:34 +0530

    done : main page of music

--- spotify_app/src/pages/Discover.jsx
+++ spotify_app/src/pages/Discover.jsx
@@ -25,7 +25,7 @@ const Discover = () => {
             <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
                 {
                     data?.map((song, i) => (
-                        <SongCard key={song.key} song={song} i={i} isPlaying={isPlaying} activeSong={activeSong} />
+                        <SongCard key={song.key} song={song} i={i} isPlaying={isPlaying} activeSong={activeSong} data={data} />
                     ))
                 }
             </div>
