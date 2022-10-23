19946ee86bae1a0fc2eb3237eb686d999e654991
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-10-23 18:51:35 +0530

    created : card components and built api for songs

---
+++ spotify_app/src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'fd4df3aaf5msh2fedeb1f1a2340fp1d4a69jsndcc77bdba5d1')
+            return headers
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
