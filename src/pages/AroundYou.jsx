348c5856644db99cda2c55c7d87a3ed22cf70c90
SiddhantTotade <siddhanttotade.1994@gmail.com>
2023-11-07 21:07:37 +0530

    add : workflow

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -28,7 +28,7 @@ const AroundYou = () => {
             <h2 className='font-bold text-3xl text-white text-left mt-4 mb-10'>
                 Around You <span className='font-black'>{country}</span>
             </h2>
-            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+            <div className='flex flex-wrap sm:justify-start md:justify-center justify-center gap-8'>
                 {data?.map((song, i) => (<SongCard key={song.key} song={song} isPlaying={isPlaying} activeSong={activeSong} data={data} i={i} />))}
             </div>
         </div>
